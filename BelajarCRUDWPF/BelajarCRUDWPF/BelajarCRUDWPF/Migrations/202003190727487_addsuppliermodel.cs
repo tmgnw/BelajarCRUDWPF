8a519d103311845e0321b80b1e539bda5fa8f5a3
Thomas Gunawan <thomasgunawannn@gmail.com>
2020-03-19 14:36:26 +0700

    first commit add model supplier

---
+++ BelajarCRUDWPF/BelajarCRUDWPF/BelajarCRUDWPF/Migrations/202003190727487_addsuppliermodel.cs
@@ -0,0 +1,27 @@
+﻿namespace BelajarCRUDWPF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addsuppliermodel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tbl_m_supplier",       //db dari table supplier
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nama = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);     //primary key
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.tbl_m_supplier");
+        }
+    }
+}
---
+++ BelajarCRUDWPF/BelajarCRUDWPF/BelajarCRUDWPF/Model/Supplier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarCRUDWPF.Model
+{
+    [Table("tbl_m_supplier")]       //table supplier
+    public class Supplier
+    {
+        [Key]       //definisi primary key dan auto increment
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Address { get; set; }
+        public Supplier()
+        { 
+
+        }
+        public Supplier(String nama, String address)
+        {
+            this.Nama = nama;
+            this.Address = address;
+        }
+    }
+}
